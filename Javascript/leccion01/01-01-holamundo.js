68028dcd5f1c2e4e16211863b75be78b32fd4d2a
Lucas Corvalán <lucas.emir.87@gmail.com>
2022-10-17 20:20:37 -0300

    Primeros ejercicios Javascript

---
+++ Javascript/leccion01/01-01-holamundo.js
@@ -0,0 +1,6 @@
+
+var nombre = "Lucas";
+nombre = "Lucas \nEmir"
+var apellido = "\nCorvalán";
+console.log(nombre+apellido);
+
---
+++ Javascript/leccion01/HolaMundo.js
@@ -0,0 +1,4 @@
+var nombre = "Lucas";
+nombre = "Hola mundo desde Javascript"
+
+console.log(nombre);
